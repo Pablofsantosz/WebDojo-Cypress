1ce249de3fa718b90b088c59c3a061fd7c4c9b3b
Pablo Felipe <pabloletiamene@gmail.com>
2026-03-10 23:48:53 -0300

    adiciona testes para cenários de login com senha inválida e email não cadastrado

--- web/cypress/e2e/login.cy.js
+++ web/cypress/e2e/login.cy.js
@@ -11,12 +11,38 @@ describe('Login', () => {
     cy.get('[data-cy="user-name"]')
       .should('be.visible')
       .and('have.text', 'Fernando Papito')
-      
+
     cy.get('[data-cy=welcome-message]')
       .should('be.visible')
       .and('have.text', 'Olá QA, esse é o seu Dojo para aprender Automação de Testes.')
-
+    cy.wait(5000)
     
+  })
+  it("Não deve logar com senha inválida",() => {
+    cy.viewport(1440, 900)
+    cy.visit('http://localhost:3000/')
+
+    cy.get('#email').type('papito@webdojo.com')
+    cy.get('#password').type('123')
+
+    cy.contains('button', 'Entrar').click()
+    cy.get('.title').should('be.visible').and('have.text','Acesso negado! Tente novamente.')
+    // Ou
+    cy.contains('.title', 'Acesso negado! Tente novamente.').should('be.visible')
+	     
+  })
+   it("Não deve logar com email não cadastrado",() => {
+    cy.viewport(1440, 900)
+    cy.visit('http://localhost:3000/')
+
+    cy.get('#email').type('pablo@webdojo.com')
+    cy.get('#password').type('123')
+
+    cy.contains('button', 'Entrar').click()
+    cy.get('.title').should('be.visible').and('have.text','Acesso negado! Tente novamente.')
+    // Ou
+    cy.contains('.title', 'Acesso negado! Tente novamente.').should('be.visible')
+	     
   })
 
 
